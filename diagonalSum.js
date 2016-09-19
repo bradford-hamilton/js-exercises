94020e62850f43b4fc2c6a034962371ee1f10372
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-09-18 19:45:49 -0600

    add 2nd solution to diagonalSum exercise

--- diagonalSum.js
+++ diagonalSum.js
@@ -19,3 +19,25 @@ var square2 = [[2, 5, 1, 3, 9],
 
 console.log(diagonalSum(square1));
 console.log(diagonalSum(square2));
+
+/* ***************************************************************************** */
+/* ***************************************************************************** */
+
+'use strict'
+const diagSum = (square) => square.reduce((sum, row, i) => sum + row[i], 0)
+
+let square = [
+  [1, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 1, 0],
+  [0, 0, 0, 1]
+]
+console.log(diagSum(square))
+square = [
+  [2, 5, 1, 3, 9],
+  [4, 1, 3, 0, 8],
+  [1, 0, 9, 7, 6],
+  [1, 7, 7, 3, 0],
+  [3, 3, 8, 2, 4]
+]
+console.log(diagSum(square))
