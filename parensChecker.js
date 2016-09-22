028e77a2c24c696a9076ea1dc264240f4c1731c7
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-09-21 21:05:21 -0600

    add solution for parensChecker

--- parensChecker.js
+++ parensChecker.js
@@ -21,3 +21,35 @@ console.log(parensChecker("())))("));       //->false
 console.log(parensChecker(")("));           //->false
 console.log(parensChecker("())))())("));    //->false
 console.log(parensChecker("())(()"));       //->false
+
+
+/* ********************************************************************** */
+/* ********************************************************************** */
+
+function parenthesisChecker(parens){
+  var opens = 0;
+  var closes = 0;
+  var result;
+
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] === '(') {
+      opens++;
+    } else if (parens[i] === ')') {
+      closes++;
+    }
+
+    if (parens[0] === '(' && parens[parens.length -1] === ')' && opens === closes){
+      result = true;
+    } else {
+      result = false;
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+console.log(parenthesisChecker("()(((())))"));   //->true
+console.log(parenthesisChecker("())))("));       //->false
+console.log(parenthesisChecker(")("));           //->false
+console.log(parenthesisChecker("())))())("));    //->false
+console.log(parenthesisChecker("())(()"));       //->false
