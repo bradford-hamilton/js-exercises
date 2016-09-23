87011b805037dcffcdb6d40cbf256821e7f304f2
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-09-23 17:13:53 -0600

    add solution for longestString

--- longestString.js
+++ longestString.js
@@ -13,3 +13,18 @@ function longestString(array) {
 }
 
 console.log( longestString(["truck", "sidewalk", "book", "dinasaurParty"]) );
+
+/* ************************************************************************* */
+/* ************************************************************************* */
+
+function longestString(strings) {
+  var longest = strings[0];
+  strings.forEach(function(string){
+    if (string.length > longest.length) longest = string;
+  });
+  var obj = {};
+  obj[longest] = longest.length;
+  return obj;
+}
+
+console.log(longestString(["truck", "sidewalk", "book"]));
